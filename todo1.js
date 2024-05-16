4b2c174c3cf49d303cc61458e62d8f7a4911191b
Endi Mimini <80917936+EndiMimini@users.noreply.github.com>
2024-05-16 15:05:11 +0200

    Update todo1.js

--- todo1.js
+++ todo1.js
@@ -2,23 +2,6 @@
 // Push Front
 // Given an array and an additional value, insert this value at the beginning of the array. You may use .push(), you are able do this without it though!
 
-// Pop Front
-// Given an array, remove and return the value at the beginning of the array. Prove the value is removed from the array by printing it. You may use .pop(), you are able do this without it though!
-
-// Insert At
-// Given an array, index, and additional value, insert the value into array at given index. You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val). You may use .push(), you are able do this without it though!
-
-// Examples:
-
-// BONUS: Remove At
-// Given an array and an index into array, remove and return the array value at that index. Prove the value is removed from the array by printing it. Think of popFront(arr) as equivalent to removeAt(arr,0).
-
-
-// BONUS: Swap Pairs
-// Swap positions of successive pairs of values of given array. If length is odd, do not change the final element.
-
-
-
 function addFirstElement(myArray, desiredValue) {
   for (let i = myArray.length; i > 0; i--) {
     myArray[i] = myArray[i - 1];
@@ -28,6 +11,10 @@ function addFirstElement(myArray, desiredValue) {
 }
 
 console.log(addFirstElement([2, 3, 7], 6));
+
+// Pop Front
+// Given an array, remove and return the value at the beginning of the array. Prove the value is removed from the array by printing it. You may use .pop(), you are able do this without it though!
+
 
 function popFront(myArray){
     myValue= myArray[0]
@@ -42,6 +29,9 @@ function popFront(myArray){
   console.log(popFront([1,2,3,4]))
 
 
+
+// Insert At
+// Given an array, index, and additional value, insert the value into array at given index. You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val). You may use .push(), you are able do this without it though!
   function insert(arr, index, value){
     for (i=arr.length; i>index; i--){
       temp= arr[i-1]
@@ -51,7 +41,10 @@ function popFront(myArray){
     return arr
   }
   console.log(insert([4,7,3,9], 1, 0))
+// Examples:
 
+// BONUS: Remove At
+// Given an array and an index into array, remove and return the array value at that index. Prove the value is removed from the array by printing it. Think of popFront(arr) as equivalent to removeAt(arr,0).
   function remove(arr, index){
     val= arr[index]
     for (i=index; i<arr.length; i++){
@@ -64,6 +57,10 @@ function popFront(myArray){
     return arr
   }
   console.log(remove([4,7,3,9], 1))
+
+// BONUS: Swap Pairs
+// Swap positions of successive pairs of values of given array. If length is odd, do not change the final element.
+
 
   function swap(arr){
     for (i=0; i<arr.length; i+=2){
@@ -80,19 +77,4 @@ function popFront(myArray){
   var array1 = [4,7,3,9]
   console.log(swap(array1))
   
-  
-  function swap2(arr){
-    var counter = 0
-    for (i=0; i<arr.length; i++){
-      if (arr[i] === arr[i+1]){
-        arr[i+1] = arr[i+2]
-        counter++
-      }
-      arr.length= arr.length-counter
-    }
-    return arr
-  }
-  
-  var array2 = ['Nicole', 'Justin', 'Endi', 'Endi', 'Flogert', 'Angela', 'Angela', 'Mario']
-  console.log(swap2(array2))
-    
+
