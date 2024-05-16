c79f2119d4f03430ff87b3b301cda8427e088467
Endi Mimini <80917936+EndiMimini@users.noreply.github.com>
2024-05-16 15:20:54 +0200

    Create todo2.js

---
+++ todo2.js
@@ -0,0 +1,13 @@
+// requirement
+// Push Front
+// Given an array and an additional value, insert this value at the beginning of the array. You may use .push(), you are able do this without it though!
+
+function addFirstElement(myArray, desiredValue) {
+  for (let i = myArray.length; i > 0; i--) {
+    myArray[i] = myArray[i - 1];
+  }
+  myArray[0] = desdValue;
+  return myArray;
+}
+
+console.log(addFirstElement([2, 3, 7], 6));
